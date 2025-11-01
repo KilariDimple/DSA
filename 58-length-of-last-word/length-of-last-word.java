015cc83c758c482161f983de402ff0803334ad94
K DIMPLE KUMAR <149712901+KilariDimple@users.noreply.github.com>
2025-11-02 01:00:49 +0530

    Time: 2 ms (9.79%) | Memory: 43.2 MB (5.01%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,21 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         s=s.trim();
-        int last=s.lastIndexOf(' ');
-        int fin=s.length()-last-1;
-        return fin;
+        // int last=s.lastIndexOf(' ');
+        // int fin=s.length()-last-1;
+        // return fin;
+int count=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            
+            if(ch!=' '){
+                count++;
+            }
+            else{
+                count=0;
+            }
+
+        }
+        return count;
     }
 }
