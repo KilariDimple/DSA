b08af3fa5ca8d6b43eb30e071f79a4186d28e12e
K DIMPLE KUMAR <149712901+KilariDimple@users.noreply.github.com>
2026-06-27 19:52:26 +0530

    Time: 15 ms (61.92%) | Memory: 51.7 MB (19.14%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l=0;
+        int r=0;
+        int cnt=0;
+        while(l < g.length && r < s.length){
+            if(g[l]<=s[r]){
+                l++;
+                r++;
+                cnt+=1;
+            }
+            // l++;
+            else{
+                r++;
+            }
+        }
+        return cnt;
+    }
+}
