c7e92ff816c54c71fe369e6a4e1aa5fa807fa402
K DIMPLE KUMAR <149712901+KilariDimple@users.noreply.github.com>
2026-07-03 15:18:10 +0530

    Time: 1 ms (6.86%) | Memory: 43.5 MB (14.94%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -13,18 +13,85 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         // List<Integer> result=new ArrayList<>();
+//         // if(root==null) return result;
+//         // Stack<TreeNode> st=new Stack<>();
+//         // st.push(root);
+//         // // TreeNode curr=st.pop();
+//         // while(!st.isEmpty()){
+//         //     TreeNode curr=st.pop();
+//         //     result.add(curr.val);
+//         //     if(curr.right!=null) st.push(curr.right);
+//         //     if(curr.left!=null) st.push(curr.left);
+//         // }
+//         // return result;
+        
+//         List<Integer> result=new ArrayList<>();
+//         if(root==null) return result;
+//             result.add(root.val);
+//             preorderTraversal(root.left);
+//             preorderTraversal(root.right);
+//             return result;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public List<Integer> preorderTraversal(TreeNode root) {
+//     List<Integer> result = new ArrayList<>();
+//     helper(root, result);
+//     return result;
+// }
+
+// private void helper(TreeNode root, List<Integer> result) {
+//     if (root == null) return;
+
+//     result.add(root.val);
+//     helper(root.left, result);
+//     helper(root.right, result);
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result=new ArrayList<>();
+        List<Integer>  result=new ArrayList<>();
         if(root==null) return result;
-        Stack<TreeNode> st=new Stack<>();
-        st.push(root);
-        // TreeNode curr=st.pop();
-        while(!st.isEmpty()){
-            TreeNode curr=st.pop();
-            result.add(curr.val);
-            if(curr.right!=null) st.push(curr.right);
-            if(curr.left!=null) st.push(curr.left);
+        Stack<TreeNode> s=new Stack<>();
+        
+        s.push(root);
+        while(!s.isEmpty()){
+            TreeNode ans=s.pop();
+            result.add(ans.val);
+            if(ans.right!=null) s.push(ans.right);
+            if(ans.left!=null) s.push(ans.left);
+
         }
         return result;
     }
