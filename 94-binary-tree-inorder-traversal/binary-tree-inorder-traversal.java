9ee7c23a194ded71725b5906e30a57a291fbd02c
K DIMPLE KUMAR <149712901+KilariDimple@users.noreply.github.com>
2026-07-03 16:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (32.50%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,31 +1,74 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// import java.util.*;
+
+// class Solution {
+    
+//     public List<Integer> inorderTraversal(TreeNode root) {
+        // List<Integer> result = new ArrayList<>();
+        // Stack<TreeNode> stack = new Stack<>();
+        
+        // TreeNode curr = root;
+        
+        // while (curr != null || !stack.isEmpty()) {
+            
+        //     // go left as much as possible
+        //     while (curr != null) {
+        //         stack.push(curr);
+        //         curr = curr.left;
+        //     }
+            
+        //     // process node
+        //     curr = stack.pop();
+        //     result.add(curr.val);
+            
+        //     // go right
+        //     curr = curr.right;
+        // }
+        
+        // return result;
+
+
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
+    
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result=new ArrayList<>();
-        Inorder(root, result);
+        Helper(root,result);
         return result;
-        
     }
-    public void Inorder(TreeNode root,List<Integer>result){
-        if(root==null){
-            return;
-        }
-        Inorder(root.left,result);
+    public void Helper(TreeNode root,List<Integer> result){
+        if(root==null) return;
+        Helper(root.left,result);
         result.add(root.val);
-        Inorder(root.right,result);
+        Helper(root.right,result);
+
     }
 }
