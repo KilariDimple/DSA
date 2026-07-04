d47ced262f78f0f8f8f6cad48518b7d45415df1e
K DIMPLE KUMAR <149712901+KilariDimple@users.noreply.github.com>
2026-07-04 16:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (84.16%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,34 +13,51 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+        
+        
+//     }
+// }
+// class Solution {
+//     public ArrayList<Integer> rightSideView(TreeNode root) {
+//         // code here
+//         ArrayList<Integer> result=new ArrayList<>();
+//         // Queue<TreeNode> q=new LinkedList<>();
+        
+//         Queue<TreeNode> q = new LinkedList<>();
+//         if(root==null) return result;
+//         q.offer(root);
+//         while(!q.isEmpty()){
+//             int  len=q.size();
+//             for(int i=0;i<len;i++){
+//                 TreeNode cur=q.poll();
+//                 if(i==0) result.add(cur.val);
+//                 if(cur.right!=null) q.offer(cur.right);
+//                 if(cur.left!=null) q.offer(cur.left);
+//                 // if(cur.right!=null) q.offer(cur.right);
+//             }
+//         }
+//         return result;
+//     }
+    
+// }
+
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-            int size = q.size(); 
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = q.poll();
-
-                if (i == 0) {   
-                    res.add(cur.val);
-                }
-                    if (cur.right != null) {
-                    q.offer(cur.right);   
-                }
-
-                if (cur.left != null) {
-                    q.offer(cur.left);
-                }
-                // if (cur.right != null) {
-                //     q.offer(cur.right);   
-                // }
-            }
-        }
+        List<Integer> res=new ArrayList<>();
+        // Stack<TreeNode> st=new Stack<>();
+        rightside(root, res,0);
         return res;
     }
+    public void rightside(TreeNode curr,List<Integer>res,int depth){
+        if(curr==null) return;
+        if(depth==res.size()){
+            res.add(curr.val);
+        }
+        rightside(curr.right,res,depth+1);
+        rightside(curr.left,res,depth+1);
+    }
+
 }
